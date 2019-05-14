06a75513a748362440bd710f2b7ecf8b516f2b2d
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-05-14 13:43:07 +0100

    adding docs

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Hello world
+# What is this?
+
+Installing this tool will provide you with the command ```cfn-docs```.
+
+The command ```cfn-docs``` accepts an AWS CloudFormation template and returns a description of the 
+template in markdown:
+
+```bash
+cfn-docs org-bootstrap.template.yaml > org-bootstrap.template.README.md
+```
+
+In the example above we redirect the std out into a file.
+
+## What does it read?
+The tool will read the description from the template, each parameter, each resource and each output.  It will output
+those descriptions in a markdown document under suitable headers.  It also lists parameters, resources and outputs 
+that do not have any descriptions.
--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("cloudformation_docs/requirements.txt", "r") as fh:
+    requirements = fh.read().split("\n")
+
 setuptools.setup(
     name="cloudformation-docs",
-    version="0.1.0",
+    version="0.1.1",
     author="Eamonn Faherty",
     author_email="packages@designandsolve.co.uk",
     description="Generate docs from cloudformation templates",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/eamonnfaherty/cloudformation-javadocs",
+    url="https://github.com/eamonnfaherty/cloudformation-doc-generator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,9 +25,5 @@ setuptools.setup(
         'console_scripts': [
             'cfn-docs = cloudformation_docs.cli:generate'
         ]},
-    install_requires=[
-        'click',
-        'Jinja2>=2.10.1',
-        'cfn_flip',
-    ],
+    install_requires=requirements,
 )
