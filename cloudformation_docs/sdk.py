21840e901867797d08df2d41fa3d1ae1be3f3f76
Eamonn Faherty <eamonnf@amazon.co.uk>
2020-08-10 15:13:58 +0100

    cleaning up

--- cloudformation_docs/sdk.py
+++ cloudformation_docs/sdk.py
@@ -1,6 +1,7 @@
 from . import core
 import json
-from cfn_flip import yaml
+from cfn_tools import load_yaml
+
 
 def generate_from_json(template_content, name):
     result = core.generate(json.loads(template_content), name)
@@ -8,5 +9,5 @@ def generate_from_json(template_content, name):
 
 
 def generate_from_yaml(template_content, name):
-    result = core.generate(yaml.load_yaml(template_content), name)
+    result = core.generate(load_yaml(template_content), name)
     return result
